55d4375fbcd4f7243c0dc30da4d8913a146b9224
Lorena Torres <lorenat0210@gmail.com>
2025-10-27 10:54:15 -0500

    MEMBER

---
+++ member_logyca/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'MEMBERS - LOGYCA ',
+    'summary': 'Description',
+    'version': '1.1',
+    'category': 'Accounting/Accounting',
+    'website': 'https://logyca.com',
+    'author': 'LOGYCA',
+    'license': 'LGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'logyca',
+        'contacts',
+        'sale',
+        'website_sale',
+        ],
+    'description': '''
+
+========================
+
+''',    
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/sale_order_template.xml',
+        'views/res_partner_view.xml',
+        'views/sale_order_view.xml',
+        'views/checkout_templates_view.xml',
+        'views/sale_order_loyalty_template.xml',
+        'wizard/member_tyb_wizard_view.xml',
+    ],
+    'qweb': [
+    ]
+}
---
+++ member_logyca/models/res_partner.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+
+from datetime import date
+from dateutil.relativedelta import relativedelta
+from odoo import api, fields, models, _
+
+from odoo.exceptions import ValidationError
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    free_member_association = fields.Boolean(string='Free Member',
+        help="Select if you want to give free membership.", default=False)
+    date_init_member_test = fields.Date(string='Inicio Periodo de Prueba Membresía', default=fields.Date.context_today)
+    date_end_member_test = fields.Date(string='Final Periodo de Prueba Membresía', default=fields.Date.context_today)
+    rango_type = fields.Selection([
+        ('comercio', 'COMERCIO - DE $1 A $2.229.451.431'),
+        ('manu', 'MANUFACTURA - DE $1 A $1.173.413.837'),
+        ('service', 'SERVICIO - DE $1 A  $1.642.769.412'),
+        ('other', 'MAYOR A  $2.229.451.431'),
+    ], string="Rango de Ingresos")
+    how_findout = fields.Selection([
+        ('asesor1', 'Asesor Naomis Murgas'),
+        ('asesor2', 'Asesor Cristian Andres Vargas'),
+        ('web', 'Sitio Web'),
+        ('aliado', 'Aliado'),
+    ], string="Cómo te enteraste?")
+
+    def activ_member_tyb(self):
+        return {
+            'name': 'Activar Membresía TYB',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': self.env.ref('member_logyca.member_tyb_wizard_view_form').id,
+            'res_model': 'member.tyb.wizard',
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+            'context': self._context
+        }
